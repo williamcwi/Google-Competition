ba404edbdd51831640cda53261ceb2a6f2393434
williamcwi <williamcwi.chu@hotmail.com>
2022-04-13 16:28:27 +0100

    added 2021 round 2 minimum sort attempt

---
+++ 2021/Round 2/minimum_sort.py
@@ -0,0 +1,23 @@
+def query(start, end):
+    print('M {} {}'.format(start, end))
+
+def swap(pos1, pos2):
+    print('S {} {}'.format(pos1, pos2))
+
+def solve(n):
+    for i in range(1, n):
+        start = i
+        end = n
+        query(start, end)
+        min = int(input())
+        if min is not i:
+            swap(min, i)
+    print('D')
+    result = int(input())
+    if result is not 1:
+        quit()
+
+if __name__ == '__main__':
+    t, n = list(map(int, input().split()))
+    for case in range(1, t+1):
+        solve(n)
